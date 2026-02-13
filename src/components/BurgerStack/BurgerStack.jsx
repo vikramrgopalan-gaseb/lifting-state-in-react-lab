d2b36b56998a6cf6902436a324e50f32a1a85ec0
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-02-12 18:34:52 -0700

    2/12 v1

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -2,12 +2,10 @@
 
 const BurgerStack = (props) => {
   return <ul className="burger-stack">
-    
-     if ( {props.stack.length === 0 ) (
+      {props.stack.length === 0 ?  (
         <p>No Ingredients</p>
-      )
-      
-      else (
+      ) :
+      (
         props.stack.map((ingredient, index) => (
           <li 
             key={index} 
